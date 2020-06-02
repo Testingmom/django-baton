1bc0a854c7844cec1c5f69b160ea85628decd065
rymck <33759092+rymck@users.noreply.github.com>
2020-06-02 12:54:12 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup
 
 with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
-    README = readme.read()
+    README = readme.read().encode('utf-8')
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
